ed5c001e38819104ec705a08c574120be243f463
immprada <im.mprada@gmail.com>
2024-04-01 10:03:17 -0500

    🚧 Add routes for registered_urls
    :index, :create, :destroy

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'temporary-session' => 'temporary_session#create'
+      resources :registered_urls, path: 'registered-urls', only: %i[create index destroy]
     end
   end
 
