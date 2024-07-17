fae5f80732a4f4139ce32329c90cbbb6b33b7e54
immprada <im.mprada@gmail.com>
2024-07-17 15:45:14 -0500

    validates temporary_session.registered_urls max length on TemporarySession model

--- app/models/temporary_session.rb
+++ app/models/temporary_session.rb
@@ -1,9 +1,13 @@
 class TemporarySession < ApplicationRecord
+  MAX_TEMPORARY_SESSION_URLS = 10
+
   before_validation :generate_uuid, on: :create
 
   validates :uuid, presence: true, uniqueness: true
 
   has_many :registered_urls, dependent: :nullify
+
+  validates :registered_urls, length: { maximum: MAX_TEMPORARY_SESSION_URLS }
 
   private
 
--- spec/models/temporary_session_spec.rb
+++ spec/models/temporary_session_spec.rb
@@ -3,6 +3,30 @@ require 'rails_helper'
 RSpec.describe TemporarySession, type: :model do
   subject(:temporary_session) { create(:temporary_session) }
   describe 'validations' do
+    describe 'when reached the maximum number of registered urls' do
+      before do
+        10.times do
+          temporary_session.registered_urls.create(url: 'http://example.com')
+        end
+      end
+
+      it 'does not allow to create a new registered url' do
+        expect(temporary_session).to be_valid
+      end
+    end
+
+    describe 'when the maximum number of registered urls is exceeded' do
+      before do
+        11.times do
+          temporary_session.registered_urls.create(url: 'http://example.com')
+        end
+      end
+
+      it 'does not allow to create a new registered url' do
+        expect(temporary_session).to be_invalid
+      end
+    end
+
     describe 'when creating a new TemporarySession' do
       it 'adds a UUID to the record' do
         expect(temporary_session.uuid).to be_present
