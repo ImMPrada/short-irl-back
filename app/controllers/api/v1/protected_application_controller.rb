6a586265d9c6a4b4d2dc881b98c656a03185c160
immprada <im.mprada@gmail.com>
2024-07-17 18:10:53 -0500

    update ProtectedApplicationController in order to get current_user based on token, or the temporary_session

--- app/controllers/api/v1/protected_application_controller.rb
+++ app/controllers/api/v1/protected_application_controller.rb
@@ -2,21 +2,47 @@ module Api
   module V1
     class ProtectedApplicationController < ApplicationController
       before_action :authenticate_request
+      include ActionController::Cookies
 
       private
 
       def authenticate_request
-        return if token || temporary_session
+        return if request.headers['Authorization'].present?
 
         render json: { error: 'Unauthorized' }, status: :unauthorized
       end
 
       def token
-        @token ||= request.headers['Authorization']&.split(' ')&.last
+        return unless bearer_token?
+
+        @token ||= request.headers['Authorization'].split('Bearer ')&.last
+      end
+
+      def current_user
+        return unless token
+
+        jwt_payload = Warden::JWTAuth::TokenDecoder.new.call(token)
+        @current_user ||= User.find(jwt_payload['sub'])
+      end
+
+      def temporary_session_token
+        return unless temporary_session_token?
+
+        @temporary_session_token ||= request.headers['Authorization']&.split('Token ')&.last
       end
 
       def temporary_session
-        @temporary_session ||= TemporarySession.find_by(uuid: token)
+        return unless temporary_session_token
+
+        @temporary_session ||= TemporarySession.find_by(uuid: temporary_session_token)
+      end
+
+      def temporary_session_token?
+        request.headers['Authorization'].split('Token ').size > 1
+      end
+
+      def bearer_token?
+        request.headers['Authorization'].split('Bearer ').size > 1
       end
     end
   end
